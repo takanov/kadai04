c48420ce4f36413077371211619f5fde9b334ede
takanov <t.taka.creative@gmail.com>
2023-06-03 01:53:09 +0900

    reademeの記載、ローカル保存したものを取り出し復元できるように調整

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-//Web Speech APIの定義
-const speech = new webkitSpeechRecognition();
-//日本語の設定
-speech.lang = 'ja-JP';
+// //Web Speech APIの定義
+// const speech = new webkitSpeechRecognition();
+// //日本語の設定
+// speech.lang = 'ja-JP';
 
 //要素を定義
 const start_btn = document.getElementById("start_btn");
@@ -20,57 +20,57 @@ function formatDate(date) {
 }
 
 
-console.log(formatDate(date)); // => "2006/01/02"
+// console.log(formatDate(date)); // => "2006/01/02"
 
 
-start_btn.addEventListener('click', function(){
-    //音声認識をスタート
-    speech.start();
-});
-
-
-
-$("#save").on('click', function(){
-    //入力された文字を取得したいと思います
-    const key = $("#key").text();
-    console.log(key,'keyの中身');
-
-    //textareaの入力箇所も取得
-    const value = $("#memo").val();
-    console.log(value, 'valueの中身');
-
-    //ローカルストレージに保存する
-    localStorage.setItem(key, value);
-
-    const html = `
-        <tr>
-            <th>${key}</th>
-            <td>${value}</td>
-        </tr>
-
-    `// テンプレートリテラル
-    $("#list").append(html);
-    $("#key").text("");
-    $("#memo").val("");
-})
+// start_btn.addEventListener('click', function(){
+//     //音声認識をスタート
+//     speech.start();
+// });
 
 
 
+// $("#save").on('click', function(){
+//     //入力された文字を取得したいと思います
+//     const key = $("#key").text();
+//     console.log(key,'keyの中身');
 
-//音声自動文字起こし機能
-speech.onresult = function(e) {
-    speech.stop();
-    if(e.results[0].isFinal) {
-        let autotext = e.results[0][0].transcript;
-        content.innerHTML +=  autotext;
-        time.innerHTML += formatDate(date);
-    }
-}
+//     //textareaの入力箇所も取得
+//     const value = $("#memo").val();
+//     console.log(value, 'valueの中身');
 
-speech.onend = () =>
-{
-    speech.start()
-};
+//     //ローカルストレージに保存する
+//     localStorage.setItem(key, value);
+
+//     const html = `
+//         <tr>
+//             <th>${key}</th>
+//             <td>${value}</td>
+//         </tr>
+
+//     `// テンプレートリテラル
+//     $("#list").append(html);
+//     $("#key").text("");
+//     $("#memo").val("");
+// })
+
+
+
+
+// //音声自動文字起こし機能
+// speech.onresult = function(e) {
+//     speech.stop();
+//     if(e.results[0].isFinal) {
+//         let autotext = e.results[0][0].transcript;
+//         content.innerHTML +=  autotext;
+//         time.innerHTML += formatDate(date);
+//     }
+// }
+
+// speech.onend = () =>
+// {
+//     speech.start()
+// };
 
 
 
@@ -84,7 +84,6 @@ const defaultBoards = [
         "class": "task",
         "item": [
             { "title": "報告書の作成" },
-            { "title": "14時から打ち合わせ" }
         ]
     },
     //進行中
@@ -109,13 +108,24 @@ const savedData = localStorage.getItem(formatDate(date));
 if (savedData) {
     const parsedData = JSON.parse(savedData);
     // 復元したデータを用いてkanbanオブジェクトを作成
-    let kanban = new jKanban({
-        // ...その他の設定...
+    kanban = new jKanban({
+        element:'#myKanban', //タスク管理ボードを表示させたいhtml要素
+        gutter: '15px', //ボード同士の間隔
+        widthBoard: '250px', //ボードサイズ
         boards: [
             { id: 'todo', title: 'タスク', class: 'task', item: parsedData.todo },
             { id: 'working', title: '進行中', class: 'progress', item: parsedData.working },
             { id: 'done', title: '完了', class: 'done', item: parsedData.done },
         ],
+        itemAddOptions: {
+            enabled: true,                                              //  ボードにボタンを追加し、アイテム作成が簡単にできるようにする
+            content: '+',                                                // ボードボタンのテキストまたはhtmlコンテンツ  
+            class: 'kanban-title-button btn btn-default btn-xs',         // ボタンのデフォルトクラス
+            footer: false                                                // ボタンをフッターに配置する
+        },  
+        buttonClick : (el, boardId) => addFormElement(boardId),
+        //要素を削除
+        click: (el) => kanban.removeElement(el),
     });
 } else {
     kanban = new jKanban({
@@ -141,7 +151,7 @@ if (savedData) {
 function addFormElement(boardId) {
     const formItem = document.createElement('form');
     formItem.setAttribute("class", "itemform");
-    formItem.innerHTML = '<div class="form-group"><textarea class="form-control" rows="2" autofocus></textarea></div><div class="form-group"><button type="submit" class="btn btn-primary btn-xs pull-right">Submit</button><button type="button" id="CancelBtn" class="btn btn-default btn-xs pull-right">Cancel</button></div>';
+    formItem.innerHTML = '<div class="form-group"><textarea class="form-control" rows="2"></textarea></div><div class="form-group"><button type="submit" class="btn btn-primary btn-xs pull-right">Submit</button><button type="button" id="CancelBtn" class="btn btn-default btn-xs pull-right">Cancel</button></div>';
 
     kanban.addForm(boardId, formItem);
     formItem.addEventListener('submit', (e) => {
@@ -163,9 +173,9 @@ function addFormElement(boardId) {
 //ボードの状態を保存
 function saveBoardState() {
     const boardData = {
-        todo: kanban.getBoardElements('todo'),
-        working: kanban.getBoardElements('working'),
-        done: kanban.getBoardElements('done'),
+        todo: Array.from(kanban.getBoardElements('todo')).map(el => ({ title: el.innerText })),
+        working: Array.from(kanban.getBoardElements('working')).map(el => ({ title: el.innerText })),
+        done: Array.from(kanban.getBoardElements('done')).map(el => ({ title: el.innerText })),
     };
     console.log('Saving board data:', boardData);
     localStorage.setItem(formatDate(date), JSON.stringify(boardData));
